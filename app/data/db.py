7d1df51d0f2e15304ba5aaa76121cca88ddcedbd
Mishti <mishtimudhoo27@gmail.com>
2025-12-03 01:08:21 +0400

    Week 9-streamlit with home page and dashboard successful

--- app/data/db.py
+++ app/data/db.py
@@ -5,6 +5,11 @@ import pandas as pd
 DB_PATH = Path("DATA") / "intelligence_platform.db"
 
 def connect_database(db_path=DB_PATH):
-    """Connect to SQLite database."""
-    return sqlite3.connect(str(db_path))
+    
+    #check if data folder exists, else create data folder
+    if not db_path.parent.exists():
+        db_path.parent.mkdir(parents=True, exist_ok=True)
+        print(f"Created the data folder")
 
+    print(f"Connecting to database at: {db_path}")
+    return sqlite3.connect(str(db_path))
